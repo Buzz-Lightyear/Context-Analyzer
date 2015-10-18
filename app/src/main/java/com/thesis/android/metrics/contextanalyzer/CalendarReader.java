f95a1ccf0f426d63ad293d3b7315dbb028ad98dd
srinivasmuthukrishnan <srinivas.muthu@rockets.utoledo.edu>
2015-10-18 12:03:09 -0400

    Intermediate, progress on filtering events by date

--- app/src/main/java/com/thesis/android/metrics/contextanalyzer/CalendarReader.java
+++ app/src/main/java/com/thesis/android/metrics/contextanalyzer/CalendarReader.java
@@ -3,10 +3,12 @@ package com.thesis.android.metrics.contextanalyzer;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.SystemClock;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -38,12 +40,24 @@ public class CalendarReader
         endDates.clear();
         descriptions.clear();
 
+        Date currentDate = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat(
+                "dd/MM/yyyy hh:mm:ss a");
+
+        Date tomorrow = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(tomorrow);
+        calendar.add(Calendar.DATE, 1);
+        tomorrow = calendar.getTime();
+        String tomorrowDate = formatter.format(tomorrow);
+
         for (int i = 0; i < calendarNames.length; i++) {
 
-            events.add(cursor.getString(1));
-            descriptions.add(cursor.getString(2));
-            startDates.add(getDate(Long.parseLong(cursor.getString(3))));
-            endDates.add(getDate(Long.parseLong(cursor.getString(4))));
+            String date = formatter.format(currentDate);
+            String eventDate = getDate(Long.parseLong(cursor.getString(3)));
+
+            if(eventDate.compareTo(date) >= 0 && eventDate.compareTo(tomorrowDate) <= 0)
+                events.add(cursor.getString(1));
             cursor.moveToNext();
         }
 
